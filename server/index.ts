6e9cd3687a0b25bc37297e345a911265bc828066
diweesomchi <41409414-diweesomchi@users.noreply.replit.com>
2025-05-03 14:05:48 +0000

    Restored to 'f6af57605d899e1de763c556381c58820571fe78'
    Replit-Restored-To: f6af57605d899e1de763c556381c58820571fe78

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,8 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import cors from "cors";
 
 const app = express();
-
-// Configure CORS middleware
-app.use(cors({
-  origin: ['http://localhost:5000', 'https://vyna.live', 'chrome-extension://*', 'moz-extension://*'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
